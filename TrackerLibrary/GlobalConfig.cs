c9fa82b0399fc4ff8c953670693c3e5d3d2d3e9c
cgordon <callumjosephiaingordon@gmail.com>
2023-03-31 11:25:35 +0200

    added extra functionality to the connect to database method in the GlobalConfig file

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -4,15 +4,22 @@ using System.Text;
 
 namespace TrackerLibrary
 {
+
+    /// <summary>
+    /// This class is responsible for the configurartion of database connections
+    /// </summary>
     public static class GlobalConfig
     {
-        public static List <IDataConnection> Connections { get; private set; }
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>(); 
 
         public static void InitializeConnections(bool database, bool textFiles)
         {
             if (database)
             {
                 //TODO - Create the SQL Connection
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
+
             }
 
             if (textFiles)
