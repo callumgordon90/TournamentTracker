6e1dded94ea1bc1157a29791a2031335613483de
cgordon <callumjosephiaingordon@gmail.com>
2023-04-05 13:02:44 +0200

    In the TextConnectorProcessor file the following tasks have now been completed: Load the text file and convert the text to List<PrizeModel>

--- TrackerLibrary/DataAccess/TextConnectorProcessor.cs
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
 using System.Text;
+using TrackerLibrary.Models;
+
+// * Load the text file
+// * Convert the text to List <PrizeModel>
+// Find the ID
+// Add the new record with the new ID (max + 1)
+// Convert the prizes to list<string>
+// Save the list<string> to the text file
 
 namespace TrackerLibrary.DataAccess.TextConnector
 {
-    public class TextConnectorProcessor
+    public static class TextConnectorProcessor
     {
-        public static string FullFilePath(string fileName) // PrizeModels.csv
+        public static string FullFilePath(this string fileName) // PrizeModels.csv
         {
+            // C:\TournamentTracker\PrizeModels.csv
+            return $"{ ConfigurationManager.AppSettings["FilePath"] }\\{ fileName } ";
 
-        
+        }
+
+        public static List<string> LoadFile(this string file)
+        {
+            if (!File.Exists(file))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(file).ToList(); 
+        }
+
+        public static List<PrizeModel> ConvertToPrizeModels(this List<string> lines)
+        {
+            List<PrizeModel> output = new List<PrizeModel>();
+
+            foreach (string line in lines) 
+            {
+                string[] cols = line.Split(',');
+
+                PrizeModel p = new PrizeModel();
+                p.Id = int.Parse(cols[0]);
+                p.PlaceNumber = int.Parse(cols[1]);
+                p.PlaceName = cols[2];
+                p.PrizeAmount = decimal.Parse(cols[3]);
+                p.PrizePercentage = double.Parse(cols[4]);
+                output.Add(p);
+
+            }
+
+            return output;
         }
     }
 
